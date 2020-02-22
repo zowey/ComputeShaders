31372f91134cc09416fb96c25a9794cfec5d787f
Zorana Curkovic <zorana.curkovic@zorana.curkovic-146M>
2020-02-22 20:44:42 +0000

    Init

---
+++ Tutorial/Assets/Scripts/DispatchDefault.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DispatchDefault : MonoBehaviour
+{
+    public ComputeShader computeShader;
+    public RenderTexture result;
+
+    public Renderer renderer;
+
+    void Start()
+    {
+        int kernel = computeShader.FindKernel("CSMain");
+        result = new RenderTexture(512, 512, 24);
+        result.enableRandomWrite=true;
+        result.Create();
+        
+        computeShader.SetTexture(kernel, "Result", result);
+        computeShader.Dispatch(kernel, 512/8, 512/8, 1);
+        renderer.material.SetTexture("_MainTex", result);
+    }
+}
