9e8f17ca8ed8ac59cd9522220a3e6e832c03e420
Zorana Curkovic <zorana.curkovic@zorana.curkovic-146M>
2020-02-23 18:56:30 +0000

    Added 1D and 2D work organisation preview

---
+++ Tutorial/Assets/Scripts/DIspatchTwoDim.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DIspatchTwoDim : MonoBehaviour
+{
+    public ComputeShader computeShader;
+    ComputeBuffer buffer;
+    void Start()
+    {
+        buffer = new ComputeBuffer(2 * 2 * 4 * 4, sizeof(int));
+        int kernel1 = computeShader.FindKernel("CSMain1");
+        int kernel2 = computeShader.FindKernel("CSMain2");
+
+        computeShader.SetBuffer(kernel2, "buffer2", buffer);
+        computeShader.Dispatch(kernel2, 2, 2, 1);
+
+        int[] data = new int[2 * 2 * 4 * 4];
+        buffer.GetData(data);
+
+        for (int i=0; i<8; ++i)
+        {
+            string line="";
+            for (int j=0; j<8; ++j)
+            {
+                line += " " + data[j + i*8];
+            }
+            Debug.Log(line);
+
+        }
+
+        buffer.Release();
+    }
+}
---
+++ Tutorial/Assets/Scripts/DispatchSimple.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DispatchSimple : MonoBehaviour
+{
+    public ComputeShader compute;
+   
+    void Start()
+    {
+        int kernel = compute.FindKernel("CSMain");
+
+        ComputeBuffer computeBuffer = new ComputeBuffer(2 * 4, sizeof(int));
+        compute.SetBuffer(kernel, "buffer", computeBuffer);
+
+        compute.Dispatch(kernel, 2, 1, 1);
+
+        int[] data = new int[8];
+        computeBuffer.GetData(data);
+
+        Debug.Log("[Dispatch Simple With Structured:");
+        for (int i=0; i<8; ++i)
+        {
+            Debug.Log(data[i]);
+        }
+
+        computeBuffer.Release();
+    }
+
+
+}
